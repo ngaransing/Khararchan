034312f4fa0426527b38852333b3290e5c2834f0
Ngaransing KH <albert.asing@outlook.com>
2022-11-01 17:55:47 +0530

    Add project files.

---
+++ Khararchan/Shared/Blog.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Khararchan.Shared
+{
+    internal class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
